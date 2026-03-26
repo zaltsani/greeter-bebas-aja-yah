e75af5c22d1927967b605efe187c56bb7509173f
dapruk <dapruks@gmail.com>
2026-03-26 09:06:26 +0700

    setup project basic dependencies, framework, libraries and packages

---
+++ app/lib/use-is-mobile.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile(breakpoint: number = MOBILE_BREAKPOINT) {
+  const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${breakpoint - 1}px)`);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < breakpoint);
+    };
+    mql.addEventListener('change', onChange);
+    setIsMobile(window.innerWidth < breakpoint);
+    return () => mql.removeEventListener('change', onChange);
+  }, []);
+
+  return !!isMobile;
+}
