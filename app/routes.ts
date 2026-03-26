443fc6f8ab67c89b573708c48be4f4714d05becd
dapruk <dapruks@gmail.com>
2026-03-26 09:03:18 +0700

    added routing for login and register page

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes"
+import { type RouteConfig, index, route } from "@react-router/dev/routes"
 
-export default [index("routes/home.tsx")] satisfies RouteConfig
+export default [
+  index("routes/home.tsx"),
+  route("login", "routes/login.tsx"),
+  route("register", "routes/register.tsx")
+] satisfies RouteConfig
