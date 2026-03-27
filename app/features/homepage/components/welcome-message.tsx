9056a35790e45ab2ad127b1fd01a14c078ee09cc
Cahaya Ed. <chyedriansa@gmail.com>
2026-03-27 15:03:35 +0700

    feat:add fetching data for welcome message and get name from supabase

--- app/features/homepage/components/welcome-message.tsx
+++ app/features/homepage/components/welcome-message.tsx
@@ -1,19 +1,27 @@
+import { useProfile } from "~/hooks/fetch-user";
 import { getCurrentDateFormatted } from "~/utils/get-current-date-formatted";
 import { greeting_list_definition } from "../greeting/greeting-list-definition";
 import { useGreetingSelection } from "../greeting/greeting-selection";
 
 export function WelcomeMessage() {
   const greeting = useGreetingSelection(greeting_list_definition);
-  const name = "Zaltsani Fadlillah";
-  const currentDate = getCurrentDateFormatted()
+  const currentDate = getCurrentDateFormatted();
+
+  const { data: profile, isLoading } = useProfile();
+  const displayName = isLoading ? "..." : profile?.name || "Guest";
 
   return (
-    <div className="grid gap-2 animate-in fade-in slide-in-from-left duration-700">
-      <div className="font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl grid leading-tight">
+    <div className="grid gap-2 w-full px-4 py-6 sm:px-6 sm:py-8 md:px-8 md:py-12 rounded-lg border border-primary/50 animate-in fade-in slide-in-from-left duration-700">
+      <div className="font-bold text-2xl lg:text-3xl grid">
         <p className="text-[#a04223]/80 font-medium">{greeting},</p>
-        <p className="text-[#343230]">{name}</p>
+        <p className="text-[#343230]">{displayName}</p>
       </div>
-      <p className="sm:text-lg">Today is {currentDate}. A perfect day to nurture your inner glow.</p>
+      <p className="sm:text-lg">
+        Today is {currentDate}. A perfect day to nurture your inner glow.
+      </p>
+      {isLoading && (
+        <div className="h-4 w-24 bg-gray-200 animate-pulse rounded mt-1" />
+      )}
     </div>
   );
 }
